2f2f8e42a153d26ad0e4c563d57c9f3abccd6cef
Guilerme Rauseo <grluz7@gmail.com>
2026-05-08 00:37:29 -0300

    feat(livro): metodo de editar e remover livro na classe LivroService

--- backend/src/main/java/com/papirotech/biblioteca/service/LivroService.java
+++ backend/src/main/java/com/papirotech/biblioteca/service/LivroService.java
@@ -5,6 +5,7 @@ import com.papirotech.biblioteca.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class LivroService {
@@ -26,5 +27,52 @@ public class LivroService {
 
         // salva no banco
         return livroRepository.save(livro);
+    }
+
+    public Livro editarLivro(Integer idLivro, Livro livroComNovosDados) {
+
+        Optional<Livro> caixaDoLivro = livroRepository.findById(idLivro);
+
+        if (caixaDoLivro.isEmpty()) {
+            throw new RuntimeException("LivroNaoEncontradoException: Livro não encontrado no sistema");
+        }
+
+        Livro livroExistente = caixaDoLivro.get();
+
+        livroExistente.setTitulo(livroComNovosDados.getTitulo());
+        livroExistente.setAutor(livroComNovosDados.getAutor());
+        livroExistente.setIsbn(livroComNovosDados.getIsbn());
+        livroExistente.setEditora(livroComNovosDados.getEditora());
+        livroExistente.setSinopse(livroComNovosDados.getSinopse());
+        livroExistente.setAnoPublicacao(livroComNovosDados.getAnoPublicacao());
+
+        return livroRepository.save(livroExistente);
+
+    }
+
+    public boolean removerLivro(Integer idLivro) {
+
+        Optional<Livro> caixaDoLivro = livroRepository.findById(idLivro);
+
+        if (caixaDoLivro.isEmpty()) {
+            throw new RuntimeException("LivroNaoEncontradoException: Livro não encontrado no sistema");
+        }
+
+        Livro livroExistente = caixaDoLivro.get();
+
+        if (livroExistente.getEmprestimos() != null && livroExistente.getEmprestimos().stream()
+                .anyMatch(e -> {
+                    var status = e.getStatus();
+
+                    return status != StatusEmprestimo.DEVOLVIDO
+                            && status != StatusEmprestimo.DEVOLVIDO_COM_ATRASO
+                            && status != StatusEmprestimo.CANCELADO;
+                })) {
+            return false;
+        }
+        livroRepository.delete(livroExistente);
+
+        return true;
+
     }
 }
